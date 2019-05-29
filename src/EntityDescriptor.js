525408d8852113a92a7794e8edbb5486212c0590
nao3301 <nao.lupa@gmail.com>
2019-05-29 11:29:34 +0200

    fix base is undefined

--- src/EntityDescriptor.js
+++ src/EntityDescriptor.js
@@ -1,6 +1,6 @@
 module.exports = class EntityDescriptor {
   constructor(metadata) {
-    const base = metadata.EntityDescriptor;
+    const base = metadata['md:EntityDescriptor'];
     Object.defineProperty(this, 'entityID', { value: base.entityID });
     Object.defineProperty(this, 'documentID', { value: base.ID });
   }
